34f2ffc07e831a904d52687a77a11dfb653e2aef
breakfasting <breakfast.ing@gmail.com>
2018-06-15 17:24:05 +0800

    Post Finished

--- src/NewPost.js
+++ src/NewPost.js
@@ -3,19 +3,53 @@
 import React, { Component } from 'react';
 
 class NewPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      cat: 'Website Project',
+      descr: '',
+      article: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const name = target.name;
+
+    this.setState({
+      [name]: target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    fetch('http://localhost:8080/new', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(response => response.json)
+      .then(body => console.log(body));
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="container mt-auto mb-auto d-flex justify-content-center">
         <div className="col-md-6 bg-white p-3">
           <h1>&frasl;&frasl; NewPost<span className="design">.</span></h1>
-          <form className="pt-3">
+          <form onSubmit={this.handleSubmit} className="pt-3">
             <label htmlFor="postTitle" className="w-100">
               Title
-              <input type="text" className="form-control" id="postTitle" placeholder="Example Title" />
+              <input name="title" type="text" value={this.state.title} onChange={this.handleChange} className="form-control" id="postTitle" placeholder="Example Title" />
             </label>
             <label htmlFor="postCategory" className="pt-2">
               Post Category
-              <select className="form-control" id="postCategory">
+              <select name="cat" value={this.state.cat} onChange={this.handleChange} className="form-control" id="postCategory">
                 <option>Website Project</option>
                 <option>Visual Design</option>
                 <option>Other</option>
@@ -23,11 +57,11 @@ class NewPost extends Component {
             </label>
             <label htmlFor="postDesc" className="w-100">
               Description
-              <input type="text" className="form-control" id="postDesc" placeholder="A new post description" />
+              <input name="descr" type="text" value={this.state.descr} onChange={this.handleChange} className="form-control" id="postDesc" placeholder="A new post description" />
             </label>
             <label htmlFor="postBody" className="w-100 pt-2">
               Article
-              <textarea className="form-control" id="postBody" placeholder="Example Text" rows="3" />
+              <textarea name="article" value={this.state.article} onChange={this.handleChange} className="form-control" id="postBody" placeholder="Example Text" rows="3" />
             </label>
             <button type="submit" className="btn btn-dark ml-auto mt-3">Submit</button>
           </form>
--- src/server/index.js
+++ src/server/index.js
@@ -4,8 +4,11 @@ var express = require('express');
 var app = express();
 var mysql      = require('mysql');
 var cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var connection = mysql.createConnection({
   host     : '140.122.143.108',
@@ -34,6 +37,42 @@ app.get('/posts', function (req, res) {
         res.send(rows);
     });
     // connection.end();
+});
+
+app.post('/new', function (req, res) {
+
+    res.send('post success');
+    console.log(req.body);
+    let nextID = 0;
+
+    connection.query('SELECT * FROM `blog`', function(err, rows, fields) {
+        if (err) throw err;
+        nextID = rows.length;
+        console.log(nextID);
+        const queryText = 'INSERT INTO `webprog2018`.`blog` (`postId`, `title`, `descr`, `cat`, `thumb`, `time`, `article`) VALUES ("' + 
+        nextID + 
+        '", "' + 
+        req.body.title + 
+        '", "' + 
+        req.body.descr + 
+        '", "' + 
+        req.body.cat + 
+        '", "images/project-ph.jpg", NOW(), "' + 
+        req.body.article + 
+        '")';
+        console.log(queryText);
+        connection.query(queryText, function(err, rows, fields) {
+            if (err) throw err;
+            console.log('sent');
+        });
+    });
+
+   // const queryText = 'INSERT INTO `webprog2018`.`blog` (`postId`, `title`, `descr`, `cat`, `thumb`, `time`, `article`) VALUES (' + nextID + ', ' + req.body.title + ', ' + req.body.descr + ', ' + req.body.cat + ', images/project-ph.jpg, NOW(), ' + req.body.article +')';
+    
+    connection.query(queryText, function(err, rows, fields) {
+        if (err) throw err;
+        console.log('sent');
+    });
 });
 
 app.listen(8080, function () {
